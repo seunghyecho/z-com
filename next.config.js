33bb49d3fd5a92fab38dbdc697ee46875c545f7f
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2024-05-06 16:13:54 +0900

    fix : pages 배포

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/upload/:slug",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
-      },
-    ];
-  },
+ output: "export",
+ async rewrites() {
+  return [
+   {
+    source: "/upload/:slug",
+    destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
+   },
+  ];
+ },
 };
 
 module.exports = nextConfig;
